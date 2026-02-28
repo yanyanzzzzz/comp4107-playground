61ef08abb26a8cff8d8567a544103d15b08fca47
SIN WING YAN <25206842@life.hkbu.edu.hk>
2026-02-28 15:41:45 +0800

    Quick trunk-based change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,9 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
+
     SelfIntroduction().selfIntroduction()
+    println("This is lab6 Exercise")
 
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
