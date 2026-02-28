36d035aff5c6b723b05f9b94dacf051928cb45ca
SIN WING YAN <25206842@life.hkbu.edu.hk>
2026-02-28 15:21:03 +0800

    Add SelfIntroduction and update Main.kt

---
+++ src/main/kotlin/sinwingyan/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.sinwingyan
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Sin Wing Yan.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
